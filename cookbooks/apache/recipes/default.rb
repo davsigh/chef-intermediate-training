21983291fe3b4c0e495455634ac996e2d669dec2
Davinder Singh <idavindersingh@gmail.com>
2021-06-28 11:00:12 +0530

    Lab05 completed

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+
+package 'httpd' do
+    action :install
+end
+file '/var/www/html/index.html' do
+    content 'Welcome Home'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
+service 'httpd' do
+    action [:start, :enable]
+end
+
+
--- cookbooks/apache/test/integration/default/default_test.rb
+++ cookbooks/apache/test/integration/default/default_test.rb
@@ -3,14 +3,16 @@
 # The Chef InSpec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe command('curl http://localhost') do
+  its(:stdout) { should match(/Welcome Home/) }
+end
+describe port('80') do
+  it { should be_listening }
+end
+describe package('httpd') do
+  it { should be_installed}
+end
+describe file('/var/www/html/index.html') do
+  it { should exist }  
 end
